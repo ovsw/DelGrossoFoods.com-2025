140ba46bbdf83a387802971291ea189525d39949
Ovi Savescu <ovi@ovswebsites.com>
2025-08-29 17:08:33 +0300

    feat: add recipe and recipe category document types with structure integration
    - Introduced new document types `recipe` and `recipeCategory` for managing recipes and their categories, including fields for title, ingredients, directions, and tags.
    - Added `recipeIndex` document type for a listing page of recipes, enhancing the overall structure.
    - Updated the `documents` export to include the new types and integrated them into the structure for better organization and access.

--- apps/studio/schemaTypes/documents/index.ts
+++ apps/studio/schemaTypes/documents/index.ts
@@ -6,6 +6,9 @@ import { footer } from "./footer";
 import { homePage } from "./home-page";
 import { navbar } from "./navbar";
 import { page } from "./page";
+import { recipeIndex } from "./recipe-index";
+import { recipeCategoryType } from "./recipeCategoryType";
+import { recipeType } from "./recipeType";
 import { sauce } from "./sauce";
 import { sauceIndex } from "./sauce-index";
 import { settings } from "./settings";
@@ -14,9 +17,19 @@ export const singletons = [
   homePage,
   blogIndex,
   sauceIndex,
+  recipeIndex,
   settings,
   footer,
   navbar,
 ];
 
-export const documents = [blog, page, faq, author, sauce, ...singletons];
+export const documents = [
+  blog,
+  page,
+  faq,
+  author,
+  sauce,
+  recipeType,
+  recipeCategoryType,
+  ...singletons,
+];
