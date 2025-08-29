c6be2cd444f87ae07428b73d393775f3888671b5
Ovi Savescu <ovi@ovswebsites.com>
2025-08-29 15:41:56 +0300

    feat: add sauce document type and update documents list
    - Introduced a new document type `sauce` with fields for basic info, images, nutritional information, and author details.
    - Updated the `documents` export to include the new `sauce` type.

---
+++ apps/studio/schemaTypes/documents/sauce.ts
@@ -0,0 +1,249 @@
+import {
+  BarChartIcon,
+  DocumentTextIcon,
+  ImageIcon,
+  UserIcon,
+} from "@sanity/icons";
+import type { StringOptions } from "sanity";
+import { defineField, defineType } from "sanity";
+
+import type { AltTextFromFieldOptions } from "../../components/inputs/AltTextFromField";
+import { AltTextFromField } from "../../components/inputs/AltTextFromField";
+
+export const sauce = defineType({
+  name: "sauce",
+  title: "Sauce",
+  type: "document",
+  fieldsets: [
+    { name: "categories", title: "Categories", options: { columns: 2 } },
+  ],
+  groups: [
+    {
+      name: "basic",
+      title: "Basic Info",
+      default: true,
+      icon: DocumentTextIcon,
+    },
+    { name: "images", title: "Images", icon: ImageIcon },
+    { name: "nutritional", title: "Nutritional Info", icon: BarChartIcon },
+    { name: "author", title: "Author", icon: UserIcon },
+  ],
+  fields: [
+    defineField({
+      name: "orderRank",
+      title: "Order Rank",
+      type: "string",
+      hidden: true,
+    }),
+    // Basic
+    defineField({
+      name: "name",
+      title: "Name",
+      type: "string",
+      group: "basic",
+      validation: (rule) => rule.required().error("Name is required"),
+    }),
+    defineField({
+      name: "line",
+      title: "Product Line",
+      type: "string",
+      fieldset: "categories",
+      options: { list: ["Original", "Organic", "Ultra-Premium"] },
+      group: "basic",
+      validation: (rule) => rule.required().error("Line is required"),
+    }),
+    defineField({
+      name: "category",
+      title: "Sauce Type",
+      type: "string",
+      fieldset: "categories",
+      options: {
+        list: ["Pasta Sauce", "Pizza Sauce", "Salsa Sauce", "Sandwich Sauce"],
+      },
+      group: "basic",
+      validation: (rule) => rule.required().error("Type is required"),
+    }),
+    defineField({
+      name: "sauceJarSize",
+      title: "Jar Size",
+      type: "string",
+      options: {
+        list: ["26 oz.", "24 oz.", "16.9 oz.", "16 oz.", "14 oz.", "13.5 oz."],
+      },
+      group: "basic",
+      validation: (rule) => rule.required().error("Jar size is required"),
+    }),
+    defineField({
+      name: "description",
+      title: "Description",
+      type: "richText",
+      group: "basic",
+      validation: (rule) => rule.required().error("Description is required"),
+    }),
+    // Images
+    defineField({
+      name: "mainImage",
+      type: "image",
+      group: "images",
+      fields: [
+        defineField({
+          name: "alt",
+          readOnly: false,
+          type: "string",
+          title: "Alternative text (read-only, 🤖auto-generated)",
+          components: { input: AltTextFromField },
+          options: {
+            sourceField: "name",
+            template: "a jar of DelGrosso's ${value} sauce",
+          } as StringOptions & AltTextFromFieldOptions,
+        }),
+      ],
+    }),
+    defineField({
+      name: "labelFlatImage",
+      type: "image",
+      group: "images",
+      fields: [
+        defineField({
+          name: "alt",
+          type: "string",
+          title: "Alternative text",
+          components: { input: AltTextFromField },
+          options: {
+            sourceField: "name",
+            template: "${value} full label",
+          } as StringOptions & AltTextFromFieldOptions,
+        }),
+      ],
+    }),
+    // Nutritional
+    defineField({
+      name: "nutritionalInfo",
+      group: "nutritional",
+      title: "Nutritional Information",
+      type: "object",
+      fields: [
+        { name: "netWeight", title: "Net Weight", type: "string" },
+        {
+          name: "servingsPerContainer",
+          title: "Servings / Cont.",
+          type: "string",
+        },
+        { name: "servingSize", title: "Serving Size", type: "string" },
+        { name: "gramsPerServing", title: "Gr. / Serving", type: "string" },
+        { name: "calories", title: "Calories", type: "string" },
+        { name: "totalFat", title: "Total Fat", type: "string" },
+        { name: "totalFatPerc", title: "Total Fat Perc.", type: "string" },
+        { name: "saturatedFat", title: "Saturated Fat", type: "string" },
+        {
+          name: "saturatedFatPerc",
+          title: "Saturated Fat Perc.",
+          type: "string",
+        },
+        { name: "transFat", title: "Trans Fat", type: "string" },
+        { name: "cholesterol", title: "Cholesterol", type: "string" },
+        { name: "cholesterolPerc", title: "Cholesterol Perc.", type: "string" },
+        { name: "sodium", title: "Sodium", type: "string" },
+        { name: "sodiumPerc", title: "Sodium Perc.", type: "string" },
+        {
+          name: "totalCarbohydrate",
+          title: "Total Carbohydrate",
+          type: "string",
+        },
+        {
+          name: "totalCarbohydratePerc",
+          title: "Total Carbohydrate Perc.",
+          type: "string",
+        },
+        { name: "dietaryFiber", title: "Dietary Fiber", type: "string" },
+        {
+          name: "dietaryFiberPerc",
+          title: "Dietary Fiber Perc.",
+          type: "string",
+        },
+        { name: "totalSugars", title: "Total Sugars", type: "string" },
+        { name: "addedSugars", title: "Added Sugars", type: "string" },
+        {
+          name: "addedSugarsPerc",
+          title: "Added Sugars Perc.",
+          type: "string",
+        },
+        { name: "protein", title: "Protein", type: "string" },
+        { name: "vitaminD", title: "Vitamin D", type: "string" },
+        { name: "vitaminDPerc", title: "Vitamin D %", type: "string" },
+        { name: "calcium", title: "Calcium", type: "string" },
+        { name: "calciumPerc", title: "Calcium Perc.", type: "string" },
+        { name: "iron", title: "Iron", type: "string" },
+        { name: "ironPerc", title: "Iron Perc.", type: "string" },
+        { name: "potassium", title: "Potassium", type: "string" },
+        { name: "potassiumPerc", title: "Potassium Perc.", type: "string" },
+      ],
+    }),
+    defineField({
+      name: "ingredients",
+      type: "text",
+      group: "nutritional",
+      rows: 7,
+    }),
+    defineField({
+      name: "allergens",
+      type: "text",
+      group: "nutritional",
+      rows: 3,
+    }),
+    // Author
+    defineField({
+      name: "authorName",
+      title: "Author Name",
+      type: "string",
+      group: "author",
+      hidden: ({ document }) => {
+        const typedDocument = document as { line?: string } | undefined;
+        return typedDocument?.line !== "Ultra-Premium";
+      },
+    }),
+    defineField({
+      name: "authorImage",
+      type: "image",
+      group: "author",
+      hidden: ({ document }) => {
+        const typedDocument = document as { line?: string } | undefined;
+        return typedDocument?.line !== "Ultra-Premium";
+      },
+      fields: [
+        defineField({
+          name: "alt",
+          type: "string",
+          title: "Alternative text",
+          components: { input: AltTextFromField },
+          options: {
+            sourceField: "authorName",
+            template: "${value}",
+          } as StringOptions & AltTextFromFieldOptions,
+        }),
+      ],
+    }),
+  ],
+  preview: {
+    select: {
+      title: "name",
+      line: "line",
+      category: "category",
+      media: "mainImage",
+    },
+    prepare(selection) {
+      const { title, line, category, media } = selection as {
+        title?: string;
+        line?: string;
+        category?: string;
+        media?: unknown;
+      };
+      return {
+        title: title || "Untitled Sauce",
+        subtitle:
+          (line && category && `${line} ${category} Sauce`) || line || category,
+        media,
+      };
+    },
+  },
+});
