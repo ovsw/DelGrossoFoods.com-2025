741b59e6b3c0e6dfff1e05558986bccd166ce49b
OVIS <ovi@ovswebsites.com>
2025-09-06 13:18:47 +0300

    chore(lint-staged): update ESLint cache location in lint-staged config for apps and packages

--- apps/studio/.lintstagedrc.mjs
+++ apps/studio/.lintstagedrc.mjs
@@ -2,5 +2,7 @@ import baseConfig from "../../.lintstagedrc.base.mjs";
 
 export default {
   ...baseConfig,
-  "*.{js,jsx,ts,tsx}": ["pnpm -w dlx eslint --cache --max-warnings 0 --fix"],
+  "*.{js,jsx,ts,tsx}": [
+    "pnpm -w dlx eslint --cache --cache-location node_modules/.cache/eslint/.eslintcache --max-warnings 0 --fix",
+  ],
 };
--- apps/web/.lintstagedrc.mjs
+++ apps/web/.lintstagedrc.mjs
@@ -1,6 +1,6 @@
-import baseConfig from "../../.lintstagedrc.base.mjs";
-
 export default {
   ...baseConfig,
-  "*.{js,jsx,ts,tsx}": ["pnpm -w dlx eslint --cache --max-warnings 0 --fix"],
+  "*.{js,jsx,ts,tsx}": [
+    "pnpm -w dlx eslint --cache --cache-location node_modules/.cache/eslint/.eslintcache --fix",
+  ],
 };
