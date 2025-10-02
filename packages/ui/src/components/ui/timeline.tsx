44191d6875f923acb0e84582105552847bcff72d
OVIS <ovi@ovswebsites.com>
2025-10-02 09:25:34 +0300

    feat(web): timeline design clean-up pass 3: fix tomatoes scale issue where they become smaller towards the bottom of the timeline by switching from a  while scrolling Animation through an “on enter” animation

--- packages/ui/src/components/ui/timeline.tsx
+++ packages/ui/src/components/ui/timeline.tsx
@@ -58,23 +58,17 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
 
   return (
     <div
-      className="w-full mx-auto max-w-7xl dark:bg-neutral-950 font-sans md:px-10"
+      className="w-full mx-auto max-w-7xl dark:bg-neutral-950 md:px-10"
       ref={containerRef}
     >
-      <div className="mb-16 text-center lg:mb-24">
-        <h2 className="mb-4 text-3xl font-bold text-gray-900 lg:text-4xl">
-          Our Rich Heritage
-        </h2>
-        <p className="mx-auto max-w-2xl text-lg text-gray-600">
-          Discover the story behind America&apos;s oldest family sauce-maker and
-          our commitment to authentic Italian flavors since 1914
-        </p>
-      </div>
-      <div ref={ref} className="relative max-w-7xl mx-auto pb-20">
+      <div className="mb-16 text-center lg:mb-24"></div>
+      <div ref={ref} className="relative max-w-7xl mx-auto">
         {data.map((item, index) => (
           <div key={index} className="flex justify-start pt-10  md:gap-10">
             <div className="sticky flex flex-col md:flex-row z-40 items-center top-40 self-start max-w-xs lg:max-w-sm md:w-full">
-              <TomatoMarker />
+              <div className="flex-shrink-0">
+                <TomatoMarker />
+              </div>
               <h3 className="hidden md:block text-xl md:pl-20 md:text-5xl font-bold text-neutral-500 dark:text-neutral-500 ">
                 {item.title}
               </h3>
@@ -103,7 +97,7 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
           style={{
             height: height + "px",
           }}
-          className="absolute left-6 top-0 overflow-hidden w-[6px] -translate-x-1/2 bg-[linear-gradient(to_bottom,var(--tw-gradient-stops))] from-transparent from-[0%] via-brand-green to-transparent to-[99%]  [mask-image:linear-gradient(to_bottom,transparent_0%,black_10%,black_90%,transparent_100%)] "
+          className="absolute left-10 top-0 overflow-hidden w-[6px] -translate-x-1/2 bg-[linear-gradient(to_bottom,var(--tw-gradient-stops))] from-transparent from-[0%] via-brand-green to-transparent to-[99%]  [mask-image:linear-gradient(to_bottom,transparent_0%,black_10%,black_90%,transparent_100%)] "
         >
           <motion.div
             style={{
